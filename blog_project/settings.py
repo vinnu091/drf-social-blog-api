b3e56279e7260b2c7024b2ada2370896487e78fc
vinnu091 <vyshnavi.v2685@gmail.com>
2026-02-24 22:34:50 +0530

    fix allowed hosts for render

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-42i@+*-b8(lz*@9wn-_8=h264+xadv0o!h+en5fxhoknu6^40@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
